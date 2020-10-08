b94c81e646660bd72f10816014dd96564bceb8b8
vlad <vladkalyuzhnyu@gmail.com>
2020-10-08 12:33:43 +0300

    init

--- app/src/main/java/defpackage/mimetronome/MiApp.kt
+++ app/src/main/java/defpackage/mimetronome/MiApp.kt
@@ -6,6 +6,11 @@ import android.app.NotificationManager
 import androidx.core.app.NotificationCompat
 import com.jakewharton.threetenabp.AndroidThreeTen
 import defpackage.mimetronome.extension.isOreoPlus
+import org.acra.ACRA
+import org.acra.config.CoreConfigurationBuilder
+import org.acra.config.DialogConfigurationBuilder
+import org.acra.config.MailSenderConfigurationBuilder
+import org.acra.data.StringFormat
 import org.jetbrains.anko.notificationManager
 import timber.log.Timber
 
@@ -18,6 +23,23 @@ class MiApp : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+            ACRA.init(this, CoreConfigurationBuilder(applicationContext)
+                .setBuildConfigClass(BuildConfig::class.java)
+                .setReportFormat(StringFormat.KEY_VALUE_LIST)
+                .setEnabled(true).apply {
+                    getPluginConfigurationBuilder(MailSenderConfigurationBuilder::class.java)
+                        .setMailTo("vladkalyuzhnyu@gmail.com")
+                        .setResSubject(R.string.crash_subject)
+                        .setReportFileName("logs.txt")
+                        .setReportAsFile(true)
+                        .setEnabled(true)
+                    getPluginConfigurationBuilder(DialogConfigurationBuilder::class.java)
+                        .setResTheme(R.style.Theme_MaterialComponents_Dialog)
+                        .setResTitle(R.string.crash_title)
+                        .setResText(R.string.crash_text)
+                        .setResCommentPrompt(R.string.crash_comment)
+                        .setEnabled(true)
+                })
         }
         if (isOreoPlus()) {
             notificationManager.createNotificationChannel(
